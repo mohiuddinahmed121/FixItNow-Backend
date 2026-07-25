a2e32215f16cc05e315a6533c03d326cfb3007a2
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2026-07-25 11:33:40 +0600

    enums prisma added

---
+++ src/utils/jwt.ts
@@ -0,0 +1,26 @@
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+
+const createToken = (payload: JwtPayload, secret: string, expiresIn: SignOptions["expiresIn"]) => {
+   return jwt.sign(payload, secret, { expiresIn });
+};
+
+const verifyToken = (token: string, secret: string) => {
+   try {
+      const verifiedToken = jwt.verify(token, secret);
+
+      return {
+         success: true,
+         data: verifiedToken,
+      };
+   } catch (error: any) {
+      return {
+         success: false,
+         error: error.message,
+      };
+   }
+};
+
+export const jwtUtils = {
+   createToken,
+   verifyToken,
+};
