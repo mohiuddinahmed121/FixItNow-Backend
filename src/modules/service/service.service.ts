b3121ac25b9002ebbd02c8be9a4b4a9288d0b3b9
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2026-07-27 10:44:21 +0600

    create service section added

--- src/modules/service/service.service.ts
+++ src/modules/service/service.service.ts
@@ -1,7 +1,55 @@
 import { prisma } from "../../lib/prisma";
 import { ICreateService, IUpdateService } from "./service.interface";
 
-const createService = async (userId: string, payload: ICreateService) => {};
+const createService = async (userId: string, payload: ICreateService) => {
+   // Check technician profile
+   const technician = await prisma.technicianProfile.findUnique({
+      where: {
+         userId,
+      },
+   });
+
+   if (!technician) {
+      throw new Error("Technician profile not found");
+   }
+
+   // Check category
+   const category = await prisma.category.findUnique({
+      where: {
+         id: payload.categoryId,
+      },
+   });
+
+   if (!category) {
+      throw new Error("Category not found");
+   }
+
+   // Create service
+   const service = await prisma.service.create({
+      data: {
+         title: payload.title,
+         description: payload.description,
+         price: payload.price,
+         categoryId: payload.categoryId,
+         technicianProfileId: technician.id,
+      },
+
+      include: {
+         category: true,
+         technicianProfile: {
+            include: {
+               user: {
+                  omit: {
+                     password: true,
+                  },
+               },
+            },
+         },
+      },
+   });
+
+   return service;
+};
 
 const getAllServices = async () => {};
 
